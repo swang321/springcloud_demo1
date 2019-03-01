efba0018e8458f132c79e8f92d0f940386a96968
swang321 <978530201@qq.com>
2019-03-01 16:04:05 +0800

    0.0

--- config-client/src/main/java/com/cloudconfigclient/demo/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/cloudconfigclient/demo/configclient/ConfigClientApplication.java
@@ -3,7 +3,8 @@ package com.cloudconfigclient.demo.configclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
+
+
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigClientApplication {
